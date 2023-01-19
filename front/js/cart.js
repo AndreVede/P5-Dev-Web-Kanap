e7bcefc976ab9cf79943587e7d8f6bbeb71d9dc7
Zacharie Boisnard <mecazack@gmail.com>
2023-01-19 16:15:50 +0100

    feat: Gestion du panier depuis page produit.

---
+++ front/js/cart.js
@@ -0,0 +1,3 @@
+const urlBack = "http://localhost:3000/api/products/";
+
+var cart = [];
--- front/js/product.js
+++ front/js/product.js
@@ -46,3 +46,62 @@ fetch(urlBack + idProduct)
   .catch((error) => {
     console.error(error);
   });
+
+// gestion du panier
+
+/**
+ * Récupère les données pour créer l'élément pour le panier
+ * @returns l'élément prêt pour le panier
+ */
+const getElemProduct = function () {
+  return (elem = {
+    id: idProduct,
+    count: document.getElementById("quantity").value,
+    color: document.getElementById("colors").value,
+  });
+};
+
+/**
+ * Contrôle si la couleur est renseignée et si la quantité est supérieure à 0
+ * @returns boolean
+ */
+const testInputs = function () {
+  return (
+    document.getElementById("colors").value !== "" &&
+    Number(document.getElementById("quantity").value) > 0
+  );
+};
+
+const buttonAddToCart = document
+  .getElementById("addToCart")
+  .addEventListener("click", () => {
+    if (testInputs()) {
+      // récupération du panier
+      var cart =
+        typeof localStorage.cart !== "undefined"
+          ? JSON.parse(localStorage.cart)
+          : [];
+
+      // création de l'élément du panier
+      const elem = getElemProduct();
+
+      // vérifier la présence d'un produit de la même couleur
+      if (cart.some((e) => e.id === idProduct && e.color === elem.color)) {
+        // récupérer elem de la liste cart
+        const elemInCart = cart.find(
+          (e) => e.id === idProduct && e.color === elem.color
+        );
+        cart[cart.indexOf(elemInCart)] = {
+          id: idProduct,
+          count: (Number(elemInCart.count) + Number(elem.count)).toString(), //incrémenter
+          color: elem.color,
+        };
+      } else {
+        cart.push(elem);
+      }
+      // sauvegarde du panier
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+      alert("Veuillez renseigner une couleur et une quantité !");
+    }
+  });
